83d2f95e85e7abe8713fc3350d76136a49a1d44d
Chiorean Bogdan <chioreanbogdangabriel@gmail.com>
2022-05-25 21:21:20 +0300

    Problema internship

---
+++ src/domain/Question.tsx
@@ -0,0 +1,41 @@
+import { useState, useEffect } from "react";
+import { RenderQuestion } from "./RenderQuestion";
+
+interface QuestionProps {
+  questionNumber: number;
+  onAnswer: (count: number) => void;
+}
+/* Randomize array using Durstenfeld shuffle algorithm */
+function shuffleArray(array: string[]) {
+  const copyArray = [...array];
+  for (var i = copyArray.length - 1; i > 0; i--) {
+    var j = Math.floor(Math.random() * (i + 1));
+    var temp = copyArray[i];
+    copyArray[i] = copyArray[j];
+    copyArray[j] = temp;
+  }
+  return copyArray;
+}
+export const Question = ({ onAnswer, questionNumber }: QuestionProps) => {
+
+  const [question, setQuestion] = useState({ text: '', answers: [''], correctAnswer: '' });
+  useEffect(() => {
+    fetch("https://opentdb.com/api.php?amount=1").then(response => response.json())
+      .then(data => {
+        const isBoolean = data.type === 'boolean'
+        const parsedResponse = {
+          text: data.results[0].question,
+          correctAnswer: data.results[0].correct_answer,
+          answers: isBoolean ? ['True', 'False'] : shuffleArray([...data.results[0].incorrect_answers, data.results[0].correct_answer])
+        }
+        setQuestion(parsedResponse);
+      });
+  }, [questionNumber])
+
+
+  return (
+    question.text ?
+      <RenderQuestion question={question} onAnswer={onAnswer} />
+      : null
+  );
+}
