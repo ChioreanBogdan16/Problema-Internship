3ebf8703538c8f4c87d2445a3b69a04393e73639
ChioreanBogdan16 <chioreanbogdangabriel@gmail.com>
2022-05-26 13:32:15 +0300

    Changes

--- src/domain/RenderQuestion.tsx
+++ src/domain/RenderQuestion.tsx
@@ -3,12 +3,12 @@ import { SyntheticEvent, useEffect, useState } from "react";
 
 interface Props {
   question: { answers: string[], text: string, correctAnswer: string };
-  onAnswer: (count: number) => void
+  onAnswer: (count: number) => void//returneaza void 
 }
 export const RenderQuestion = ({ question, onAnswer }: Props) => {
   const [count, setCount] = useState(10);
   const [response, setResponse] = useState('');
-
+  const [previous, setPrevious] = useState('1');
   useEffect(() => {
     const interval = setInterval(() => {
       setCount(count => {
@@ -17,18 +17,22 @@ export const RenderQuestion = ({ question, onAnswer }: Props) => {
           setCount(10)
         }
         return count - 1
-      })
+      })//timer  , daca ajunge la 0 se trimite raspuns la test (-1) si se reseteaza counterul 
     }, 1000)
-    return () => clearInterval(interval);
-  }, [onAnswer])
+    return () => clearInterval(interval);//se oreste la 0 
+  }, [onAnswer]) //se executa useEffect de fiecare data cand se modifica onAnswer(cand se apasa pe submit)
 
-  const handleSubmit = (event: any) => {
+  const handleSubmit = (event: any) => {  //cand se apasa pe submit saceasta functie este chemata ,
     event.preventDefault()
-    setCount(10);
-    if (response === question.correctAnswer)
-      onAnswer(1)
-    else
-      onAnswer(-1)
+    console.log(response)
+      if(response==='') return null;
+      else  if(response===previous) return null;
+    setPrevious(response)
+    setCount(10);   //reseteaza counter pentru urmatoarea intrebare
+    if (response === question.correctAnswer)//verifica daca raspunsul este corect 
+      onAnswer(1)  //rasp corect +1 punct
+    else 
+      onAnswer(-1)   //rasp gresit -1 punct
   }
 
   const handleChange = (event: any) => {
@@ -38,8 +42,9 @@ export const RenderQuestion = ({ question, onAnswer }: Props) => {
 
   return (
     <>
+      <div> {question.correctAnswer}</div>
       <div>Question: {question.text}</div>
-      {question.correctAnswer}
+  
       <form onSubmit={handleSubmit}>
         {
           question.answers.map((answer, index) =>
@@ -48,7 +53,7 @@ export const RenderQuestion = ({ question, onAnswer }: Props) => {
                 name="question"
                 type='radio'
                 value={answer}
-                checked={response === answer}
+                checked={response === answer} //se foloseste pentru a nu ramane selectat butonul tip radio de la o intrebare la alta 
                 onChange={handleChange} />
               {answer}
             </div>
